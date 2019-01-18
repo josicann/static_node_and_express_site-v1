59b4c1e7a68c72ce29b30e3d512ec50d09191eac
josicann <josicann@ut.utm.edu>
2019-01-18 05:51:39 -0600

    add phrase hunter project

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const app = express();
 const projectData = require('./data.json');
 const port = process.env.PORT || 3000;
+let projects = require('./data.json');
 
 app.listen(port, () => console.log(` app listening on port ${port}!`))
 
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', projectData.projects )
-})
+    res.render('index', projects.projects[0] )
+});
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 app.get('/project', (req, res) => {
-    res.render('project');
-})
+    res.render('project', projects)
+});
